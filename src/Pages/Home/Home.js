dfec8bc7d87ee2b8a88ae228494ba62801fa4fbd
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-08-08 21:26:41 +0600

    update about me and education part

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import AboutMe from '../AboutMe/AboutMe';
+import Education from '../Education/Education';
 import Introduction from '../Introduction/Introduction';
 
 const Home = () => {
     return (
         <div>
             <Introduction/>
+            <AboutMe/>
+            <Education/> 
         </div>
     );
 };
